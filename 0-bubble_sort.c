f08e748bf04d4cf9cbcb0a9c3bf35e54adec995f
danielj32 <danieljuanito21@hotmail.com>
2020-06-13 00:38:34 +0000

    add new functions and answer Bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "sort.h"
+/**
+ * bubble_sort - Entry point
+ *@array: array of integers
+ *@size: size of array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	unsigned long int ct1, ct2, swap_array;
+
+	for (ct1 = 0; ct1 < size - 1; ct1++)
+		/* code */
+		for (ct2 = 0; ct2 < size - 1; ct2++)
+			if (array[ct2] > array[ct2 + 1])
+			{
+				swap_array = array[ct2];
+				array[ct2] = array[ct2 + 1];
+				array[ct2 + 1] = swap_array;
+				print_array(array, size);
+			}
+}
