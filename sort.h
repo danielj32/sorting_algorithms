6bc6e0953f775c91e8cb4fb317caf5bca0c1f543
danielj32 <danieljuanito21@hotmail.com>
2020-06-16 00:47:13 +0000

    add prototype include new prototype

--- sort.h
+++ sort.h
@@ -22,4 +22,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void aux_quick_sort(int *array, int l, int r, size_t size);
+int partition(int *array, int l, int r, size_t size);
+void swapping(int *array, int one, int last, size_t size);
 #endif
